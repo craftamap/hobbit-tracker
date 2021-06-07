5c8587f94c7ae2ea62533c2171ab44c901031b6b
Fabian Siegel <fabians1999@gmail.com>
2021-06-07 16:53:48 +0200

    auth_test - switched to t.Run

--- routes/auth_test.go
+++ routes/auth_test.go
@@ -19,18 +19,18 @@ import (
 
 func TestHandleLogout(t *testing.T) {
 	testMatrix := []struct {
-		description string
-		prepare     func(dbM sqlmock.Sqlmock, rc func(http.Handler) http.Handler) *http.Request
-		expect      func(description string, rr *httptest.ResponseRecorder)
+		name    string
+		prepare func(dbMock sqlmock.Sqlmock, rc func(http.Handler) http.Handler) *http.Request
+		expect  func(rr *httptest.ResponseRecorder)
 	}{
 		{
-			description: "valid cookie",
-			prepare: func(dbM sqlmock.Sqlmock, rc func(http.Handler) http.Handler) *http.Request {
+			name: "valid cookie",
+			prepare: func(dbMock sqlmock.Sqlmock, rc func(http.Handler) http.Handler) *http.Request {
 				var cookies []*http.Cookie
 				{
 					// We first create a successful login and extract it's cookie
 					secret, _ := bcrypt.GenerateFromPassword([]byte("craftamap"), 0)
-					dbM.ExpectQuery("SELECT \\* FROM \"users\"").WillReturnRows(sqlmock.NewRows([]string{"id", "username", "secret", "image"}).AddRow(1, "craftamap", secret, "image"))
+					dbMock.ExpectQuery("SELECT \\* FROM \"users\"").WillReturnRows(sqlmock.NewRows([]string{"id", "username", "secret", "image"}).AddRow(1, "craftamap", secret, "image"))
 					r, _ := http.NewRequest("POST", "/", strings.NewReader("username=craftamap&password=craftamap"))
 					r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 					rr := httptest.NewRecorder()
@@ -45,9 +45,9 @@ func TestHandleLogout(t *testing.T) {
 				r.AddCookie(cookies[0])
 				return r
 			},
-			expect: func(description string, rr *httptest.ResponseRecorder) {
+			expect: func(rr *httptest.ResponseRecorder) {
 				if rr.Result().StatusCode != http.StatusFound {
-					t.Errorf("Test \"%s\" failed because we expected StatusCode %d, but got %d", description, http.StatusFound, rr.Result().StatusCode)
+					t.Errorf("expected StatusCode %d, but got %d", http.StatusFound, rr.Result().StatusCode)
 				}
 				cookies := rr.Result().Cookies()
 				var sessionCookie *http.Cookie
@@ -63,14 +63,40 @@ func TestHandleLogout(t *testing.T) {
 				}
 			},
 		}, {
-			description: "invalid/unknown cookie",
-			prepare: func(dbM sqlmock.Sqlmock, rc func(http.Handler) http.Handler) *http.Request {
+			name: "missing cookie",
+			prepare: func(dbMock sqlmock.Sqlmock, rc func(http.Handler) http.Handler) *http.Request {
 				r, _ := http.NewRequest("POST", "/", strings.NewReader(""))
 				return r
 			},
-			expect: func(description string, rr *httptest.ResponseRecorder) {
+			expect: func(rr *httptest.ResponseRecorder) {
 				if rr.Result().StatusCode != http.StatusFound {
-					t.Errorf("Test \"%s\" failed because we expected StatusCode %d, but got %d", description, http.StatusFound, rr.Result().StatusCode)
+					t.Errorf("expected StatusCode %d, but got %d", http.StatusFound, rr.Result().StatusCode)
+				}
+				cookies := rr.Result().Cookies()
+				var sessionCookie *http.Cookie
+				for _, c := range cookies {
+					if c.Name == "session" {
+						sessionCookie = c
+					}
+				}
+
+				t.Logf("sessionCookie: %+v", sessionCookie)
+				if sessionCookie == nil || sessionCookie.Expires.After(time.Now()) {
+					t.Errorf("Expected Set-Cookie to set sessionCookie to invalid Date, but got: %+v", rr.Result().Cookies())
+				}
+			},
+		}, {
+			name: "unknown cookie",
+			prepare: func(dbMock sqlmock.Sqlmock, rc func(http.Handler) http.Handler) *http.Request {
+				r, _ := http.NewRequest("POST", "/", strings.NewReader(""))
+				r.AddCookie(&http.Cookie{
+					Value: "invalid cookie",
+				})
+				return r
+			},
+			expect: func(rr *httptest.ResponseRecorder) {
+				if rr.Result().StatusCode != http.StatusFound {
+					t.Errorf("expected StatusCode %d, but got %d", http.StatusFound, rr.Result().StatusCode)
 				}
 				cookies := rr.Result().Cookies()
 				var sessionCookie *http.Cookie
@@ -89,32 +115,34 @@ func TestHandleLogout(t *testing.T) {
 	}
 
 	for _, testCase := range testMatrix {
-		dbM, mock, err := sqlmock.New()
-		if err != nil {
-			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-		}
-		dialector := postgres.New(postgres.Config{
-			DSN:                  "sqlmock_db_0",
-			DriverName:           "postgres",
-			Conn:                 dbM,
-			PreferSimpleProtocol: true,
+		t.Run(testCase.name, func(t *testing.T) {
+			dbM, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			dialector := postgres.New(postgres.Config{
+				DSN:                  "sqlmock_db_0",
+				DriverName:           "postgres",
+				Conn:                 dbM,
+				PreferSimpleProtocol: true,
+			})
+			db, err := gorm.Open(dialector, &gorm.Config{})
+
+			store := sessions.NewCookieStore([]byte("secret"))
+			// logger, _ := test.NewNullLogger()
+			logger := logrus.New()
+
+			rc := requestcontext.New(store, db, logger, nil)
+
+			r := testCase.prepare(mock, rc)
+			rr := httptest.NewRecorder()
+
+			handler := BuildHandleLogout()
+			ctxMiddleware := requestcontext.New(store, db, logger, nil)(handler)
+			ctxMiddleware.ServeHTTP(rr, r)
+
+			testCase.expect(rr)
 		})
-		db, err := gorm.Open(dialector, &gorm.Config{})
-
-		store := sessions.NewCookieStore([]byte("secret"))
-		// logger, _ := test.NewNullLogger()
-		logger := logrus.New()
-
-		rc := requestcontext.New(store, db, logger, nil)
-
-		r := testCase.prepare(mock, rc)
-		rr := httptest.NewRecorder()
-
-		handler := BuildHandleLogout()
-		ctxMiddleware := requestcontext.New(store, db, logger, nil)(handler)
-		ctxMiddleware.ServeHTTP(rr, r)
-
-		testCase.expect(testCase.description, rr)
 	}
 
 }
@@ -122,61 +150,61 @@ func TestHandleLogout(t *testing.T) {
 func TestHandleLogin(t *testing.T) {
 
 	testMatrix := []struct {
-		description string
-		prepare     func(dbM sqlmock.Sqlmock) *http.Request
-		expect      func(description string, rr *httptest.ResponseRecorder)
+		name    string
+		prepare func(dbM sqlmock.Sqlmock) *http.Request
+		expect  func(rr *httptest.ResponseRecorder)
 	}{
 		{
-			description: "empty request body",
+			name: "empty request body",
 			prepare: func(dbM sqlmock.Sqlmock) *http.Request {
 				r, _ := http.NewRequest("POST", "/", strings.NewReader(""))
 				return r
 			},
-			expect: func(description string, rr *httptest.ResponseRecorder) {
+			expect: func(rr *httptest.ResponseRecorder) {
 				if rr.Result().StatusCode != http.StatusUnauthorized {
-					t.Errorf("Test \"%s\" failed because we expected StatusCode %d, but got %d", description, http.StatusUnauthorized, rr.Result().StatusCode)
+					t.Errorf("expected StatusCode %d, but got %d", http.StatusUnauthorized, rr.Result().StatusCode)
 				}
 			},
 		}, {
-			description: "correct username and missing password",
+			name: "correct username and missing password",
 			prepare: func(dbM sqlmock.Sqlmock) *http.Request {
 				r, _ := http.NewRequest("POST", "/", strings.NewReader("username=craftamap"))
 				r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 				return r
 			},
-			expect: func(description string, rr *httptest.ResponseRecorder) {
+			expect: func(rr *httptest.ResponseRecorder) {
 				if rr.Result().StatusCode != http.StatusUnauthorized {
-					t.Errorf("Test \"%s\" failed because we expected StatusCode %d, but got %d", description, http.StatusUnauthorized, rr.Result().StatusCode)
+					t.Errorf("expected StatusCode %d, but got %d", http.StatusUnauthorized, rr.Result().StatusCode)
 				}
 			},
 		}, {
-			description: "username missing in db",
+			name: "username missing in db",
 			prepare: func(dbM sqlmock.Sqlmock) *http.Request {
 				dbM.ExpectQuery("SELECT \\* FROM \"users\"").WillReturnRows(sqlmock.NewRows([]string{"id", "username", "secret", "image"}).AddRow(1, "craftamap", "", "image"))
 				r, _ := http.NewRequest("POST", "/", strings.NewReader("username=craftamap&password=craftamap"))
 				r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 				return r
 			},
-			expect: func(description string, rr *httptest.ResponseRecorder) {
+			expect: func(rr *httptest.ResponseRecorder) {
 				if rr.Result().StatusCode != http.StatusUnauthorized {
-					t.Errorf("Test \"%s\" failed because we expected StatusCode %d, but got %d", description, http.StatusUnauthorized, rr.Result().StatusCode)
+					t.Errorf("expected StatusCode %d, but got %d", http.StatusUnauthorized, rr.Result().StatusCode)
 				}
 			},
 		}, {
-			description: "wrong password",
+			name: "wrong password",
 			prepare: func(dbM sqlmock.Sqlmock) *http.Request {
 				dbM.ExpectQuery("SELECT \\* FROM \"users\"").WillReturnRows(sqlmock.NewRows([]string{"id", "username", "secret", "image"}).AddRow(1, "craftamap", "placeholder", "image"))
 				r, _ := http.NewRequest("POST", "/", strings.NewReader("username=craftamap&password=craftamap"))
 				r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 				return r
 			},
-			expect: func(description string, rr *httptest.ResponseRecorder) {
+			expect: func(rr *httptest.ResponseRecorder) {
 				if rr.Result().StatusCode != http.StatusUnauthorized {
-					t.Errorf("Test \"%s\" failed because we expected StatusCode %d, but got %d", description, http.StatusUnauthorized, rr.Result().StatusCode)
+					t.Errorf("expected StatusCode %d, but got %d", http.StatusUnauthorized, rr.Result().StatusCode)
 				}
 			},
 		}, {
-			description: "correct credentials",
+			name: "correct credentials",
 			prepare: func(dbM sqlmock.Sqlmock) *http.Request {
 				secret, _ := bcrypt.GenerateFromPassword([]byte("craftamap"), 0)
 				dbM.ExpectQuery("SELECT \\* FROM \"users\"").WillReturnRows(sqlmock.NewRows([]string{"id", "username", "secret", "image"}).AddRow(1, "craftamap", secret, "image"))
@@ -184,9 +212,9 @@ func TestHandleLogin(t *testing.T) {
 				r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 				return r
 			},
-			expect: func(description string, rr *httptest.ResponseRecorder) {
+			expect: func(rr *httptest.ResponseRecorder) {
 				if rr.Result().StatusCode != http.StatusFound {
-					t.Errorf("Test \"%s\" failed because we expected StatusCode %d, but got %d", description, http.StatusFound, rr.Result().StatusCode)
+					t.Errorf("expected StatusCode %d, but got %d", http.StatusFound, rr.Result().StatusCode)
 				}
 				cookies := rr.Result().Cookies()
 				var sessionCookie *http.Cookie
@@ -206,30 +234,32 @@ func TestHandleLogin(t *testing.T) {
 	}
 
 	for _, testCase := range testMatrix {
-		dbM, mock, err := sqlmock.New()
-		if err != nil {
-			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-		}
-		dialector := postgres.New(postgres.Config{
-			DSN:                  "sqlmock_db_0",
-			DriverName:           "postgres",
-			Conn:                 dbM,
-			PreferSimpleProtocol: true,
+		t.Run(testCase.name, func(t *testing.T) {
+			dbM, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			dialector := postgres.New(postgres.Config{
+				DSN:                  "sqlmock_db_0",
+				DriverName:           "postgres",
+				Conn:                 dbM,
+				PreferSimpleProtocol: true,
+			})
+			db, err := gorm.Open(dialector, &gorm.Config{})
+
+			store := sessions.NewCookieStore([]byte("secret"))
+			// logger, _ := test.NewNullLogger()
+			logger := logrus.New()
+
+			rr := httptest.NewRecorder()
+
+			r := testCase.prepare(mock)
+
+			handler := BuildHandleLogin()
+			ctxMiddleware := requestcontext.New(store, db, logger, nil)(handler)
+			ctxMiddleware.ServeHTTP(rr, r)
+
+			testCase.expect(rr)
 		})
-		db, err := gorm.Open(dialector, &gorm.Config{})
-
-		store := sessions.NewCookieStore([]byte("secret"))
-		// logger, _ := test.NewNullLogger()
-		logger := logrus.New()
-
-		rr := httptest.NewRecorder()
-
-		r := testCase.prepare(mock)
-
-		handler := BuildHandleLogin()
-		ctxMiddleware := requestcontext.New(store, db, logger, nil)(handler)
-		ctxMiddleware.ServeHTTP(rr, r)
-
-		testCase.expect(testCase.description, rr)
 	}
 }

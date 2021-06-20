dd396422c32da6189e257be32bedf5bf22f6b4cf
Fabian Siegel <fabians1999@gmail.com>
2021-06-20 18:53:26 +0200

    feed: Add link to router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/feed">Feed</router-link> |
       <template v-if="isAuthenticated">
         <router-link to="/profile/me">Profile</router-link> |
         <a href="/auth/logout">Logout</a>

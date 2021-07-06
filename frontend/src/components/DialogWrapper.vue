648801e9f302c9bd406bc0b590a404c0e62fbe0c
Fabian Siegel <fabians1999@gmail.com>
2021-07-06 17:28:01 +0200

    fix eslint

--- frontend/src/components/DialogWrapper.vue
+++ frontend/src/components/DialogWrapper.vue
@@ -5,10 +5,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "DialogWrapper",
+  name: 'DialogWrapper',
 })
 </script>
 

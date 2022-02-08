d2dd7733671a8b04bbc780c39897b81113bd8931
Fabian Siegel <fabians1999@gmail.com>
2022-02-08 22:27:02 +0100

    store: completely old store and vuex :pineapple:

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { store } from './store'
 import './registerServiceWorker'
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
 
-createApp(App).use(store).use(pinia).use(router).mount('#app')
+createApp(App).use(pinia).use(router).mount('#app')

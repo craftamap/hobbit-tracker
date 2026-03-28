5d865a4393e6287c6c9c116955fed610ba9a34a5
Fabian Siegel <fabians1999@gmail.com>
2026-03-28 11:30:21 +0100

    chore: format Login.vue

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,65 +1,67 @@
 <template>
-  <FormWrapper>
-    <form ref="form" method="POST" action="/auth/login" v-on:keyup.enter="submit">
-      <div class="welcome">
-        Welcome back!
-      </div>
-      <div>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="username"
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="password"
-        />
-      </div>
-      <div>
-        <Button @click="submit" value="Login" type="primary"/>
-      </div>
-    </form>
-  </FormWrapper>
+    <FormWrapper>
+        <form
+            ref="form"
+            method="POST"
+            action="/auth/login"
+            v-on:keyup.enter="submit"
+        >
+            <div class="welcome">Welcome back!</div>
+            <div>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="username"
+                />
+            </div>
+            <div>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                />
+            </div>
+            <div>
+                <Button @click="submit" value="Login" type="primary" />
+            </div>
+        </form>
+    </FormWrapper>
 </template>
 
 <script lang="ts">
-import { defineComponent, Ref, ref } from 'vue'
-import Button from '../components/form/Button.vue'
-import FormWrapper from '../components/form/FormWrapper.vue'
+import { defineComponent, Ref, ref } from "vue";
+import Button from "../components/form/Button.vue";
+import FormWrapper from "../components/form/FormWrapper.vue";
 
 export default defineComponent({
-  name: 'LoginView',
-  components: {
-    Button,
-    FormWrapper,
-  },
-  setup() {
-    const form: Ref<HTMLFormElement | null> = ref(null)
+    name: "LoginView",
+    components: {
+        Button,
+        FormWrapper,
+    },
+    setup() {
+        const form: Ref<HTMLFormElement | null> = ref(null);
 
-    const submit = () => {
-      console.log(form)
-      form.value?.submit()
-    }
+        const submit = () => {
+            console.log(form);
+            form.value?.submit();
+        };
 
-    return {
-      form,
-      submit,
-    }
-  },
-})
+        return {
+            form,
+            submit,
+        };
+    },
+});
 </script>
 
 <style scoped>
 .welcome {
-  font-size: 16pt;
-  text-align: center;
-  margin-bottom: 2rem;
-  color: var(--secondary-text);
-
+    font-size: 16pt;
+    text-align: center;
+    margin-bottom: 2rem;
+    color: var(--secondary-text);
 }
 </style>
